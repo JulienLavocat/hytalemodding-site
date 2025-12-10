81ac2300d49b67f880480d8d389f9982ee415cc0
Baran Mordoğan <baran@okunamayanad.com>
2025-12-11 01:58:56 +0300

    feat: add metadata for Hytale Modding layout

--- src/app/[lang]/(home)/layout.tsx
+++ src/app/[lang]/(home)/layout.tsx
@@ -1,6 +1,14 @@
 import { ViewTransition, type ReactNode } from "react";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@/lib/layout.shared";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Hytale Modding",
+  description:
+    "An unofficial community for modding Hytale, providing guides, documentation, and resources.",
+};
+
 export default async function Layout({
   params,
   children,
